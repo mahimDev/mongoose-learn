9ddad26a73ba92cd67273a79337f8ccaeccc4a71
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-06-19 00:14:23 +0600

    added some changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
+import { create } from "domain";
 import express, { Application, Request, Response } from "express";
 import { model, Schema } from "mongoose";
 
 const app: Application = express();
+
+app.use(express.json());
+
 const noteSchema = new Schema({
   title: { type: String, require: true, trim: true },
   content: { type: String, default: "" },
@@ -19,13 +23,25 @@ const noteSchema = new Schema({
 
 const Note = model(`Note`, noteSchema);
 
-app.post("/create-note", async (req: Request, res: Response) => {
-  const myNote = new Note({
-    title: "Learning mongoose",
-    content: "i am learning mongoose",
-    // date: { Date },
+app.post("/notes/create-note", async (req: Request, res: Response) => {
+  const body = req.body;
+  // const myNote = new Note({
+  //   title: "Learning mongoose",
+  //   content: "i am learning mongoose",
+  // date: { Date },
+  // });
+  // await myNote.save();
+
+  const myNote = await Note.create(body);
+  res.status(201).json({
+    success: true,
+    massage: "note create successfully",
+    note: myNote,
   });
-  await myNote.save();
+});
+// get oparetion
+app.get("/notes", async (req: Request, res: Response) => {
+  const myNote = await Note.find();
   res.status(201).json({
     success: true,
     massage: "note create successfully",
