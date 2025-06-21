6d3bd8b2e3be1e0d267d848d47ace11d19f02a31
MD Mahim <138321905+mahim7172@users.noreply.github.com>
2025-06-22 02:29:41 +0600

    added some changes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
-import { create } from "domain";
 import express, { Application, Request, Response } from "express";
-import { model, Schema } from "mongoose";
-import { Note } from "./app/models/notes.models";
 import { notesRoute } from "./app/controllers/notes.controllers";
 
 export const app: Application = express();
